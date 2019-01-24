e75350037dfa18a357732550bed2e1cfc93926f6
visualstudio <visualstudio@230-PC07>
2019-01-24 17:18:22 -0500

    5:18 pm

--- C0725769/Program.cs
+++ C0725769/Program.cs
@@ -4,12 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C0725769
+namespace weeek3_csharp
 {
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var rating = CalculateRating();
+            if (rating == 0)
+                Console.WriteLine("promote to level 1");
+
+            else { Console.WriteLine("prompote to level 2"); }
+        }
+        public int CalculateRating()
+        {
+            return 0;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var customer = new Customer();
+            //Vehicle purple = new Car();
+            //Vehicle bowling = new Car();
+            //Console.WriteLine("how many cars: {0}", Vehicle.howManyCars);
+
+
+            //Book myBook = new Book();
+            //myBook.SetTitle("a septrate peace");
+            //Console.WriteLine(myBook.GetTitle());
         }
+    }
+
+    class Vehicle
+    {
+        public static int howManyCars = 0;
+        public string Color = "Blue";
+    }
+    class Car : Vehicle
+    {
+        public String Color = "Red";
+        public Car()
+        {
+            Vehicle.howManyCars++;
+            Console.WriteLine("my color is" + Color);
+        }
+    }
+    class BookCollection
+    {
+
+    }
+    class Book : BookCollection
+    {
+        private String Title;
+        public void SetTitle(String aTitle)
+        {
+            this.Title = aTitle;
+        }
+        public String GetTitle() { return this.Title; }
     }
 }
